64d4013872e2dc27c3e70f21ca55aee73c57d4bf
Leo Baker-Hytch <leo@insidelabs.tech>
2019-08-28 21:26:10 +0200

    Add validation & coercion

---
+++ lib/coerce.ts
@@ -0,0 +1,23 @@
+import { Env, EnvSchema, EnvType } from './types';
+
+type CoercedEnv = {
+    [key: string]: boolean | number | string;
+};
+
+export function coerce<S extends EnvSchema>(env: Env, schema: S): EnvType<S> {
+    const coerced: CoercedEnv = {};
+
+    for (const [key, schemaValue] of Object.entries(schema)) {
+        const value = env[key];
+
+        if (schemaValue === Boolean) {
+            coerced[key] = value === 'true';
+        } else if (schemaValue === Number) {
+            coerced[key] = parseInt(value, 10);
+        } else {
+            coerced[key] = value;
+        }
+    }
+
+    return coerced as EnvType<S>;
+}
---
+++ lib/validate.ts
@@ -0,0 +1,23 @@
+import { Env, EnvSchema } from './types';
+
+/**
+ * Only allows exactly 'true' or 'false'
+ */
+const booleanRegExp = /true|false/;
+
+/**
+ * Only allows integers.
+ */
+const numberRegExp = /\d+/;
+
+export function validate(env: Env, schema: EnvSchema): boolean {
+    for (const [key, schemaValue] of Object.entries(schema)) {
+        if (!(key in env)) return false;
+
+        const value = env[key];
+        if (schemaValue === Boolean && !booleanRegExp.test(value)) return false;
+        if (schemaValue === Number && !numberRegExp.test(value)) return false;
+        if (schemaValue instanceof RegExp && !schemaValue.test(value)) return false;
+    }
+    return true;
+}
