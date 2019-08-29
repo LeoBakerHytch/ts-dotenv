80a46a8ce9780cfa9e9ff2409d7e639c09216cb0
Leo Baker-Hytch <leo@insidelabs.tech>
2019-08-29 12:56:59 +0200

    Add docs

--- lib/load.ts
+++ lib/load.ts
@@ -12,8 +12,30 @@ interface Options {
     overrideProcessEnv?: boolean;
 }
 
+/**
+ * Loads .env from the working directory, merges it with process.env (which takes precedence),
+ * validates against the provided schema, and coerces to the corresponding types.
+ *
+ * @param schema - Mapping from variable names to desired types
+ * @param path - Custom path, also resolved relative to working directory
+ *
+ * @throws EnvError
+ */
 export function load<S extends EnvSchema>(schema: S, path?: string): EnvType<S>;
+
+/**
+ *
+ * Loads .env from the working directory, merges it with process.env, validates against the
+ * provided schema, and coerces to the corresponding types. May specify via options: a different
+ * .env path; file encoding; whether .env overrides values from process.env.
+ *
+ * @param schema - Mapping from variable names to desired types
+ * @param options - Options object
+ *
+ * @throws EnvError
+ */
 export function load<S extends EnvSchema>(schema: S, options?: Options): EnvType<S>;
+
 export function load<S extends EnvSchema>(schema: S, pathOrOptions?: string | Options): EnvType<S> {
     const { path = '.env', encoding = 'utf-8', overrideProcessEnv = false } =
         typeof pathOrOptions === 'string' ? { path: pathOrOptions } : pathOrOptions || {};
