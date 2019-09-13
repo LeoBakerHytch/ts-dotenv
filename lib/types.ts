1a72d042b09a0d67217fdb26c30b48b59921caa3
Leo Baker-Hytch <leo@insidelabs.tech>
2019-09-13 21:38:22 +0200

    Fix schema types

--- lib/types.ts
+++ lib/types.ts
@@ -2,39 +2,9 @@ export type Env = {
     [key: string]: string | undefined;
 };
 
-export type EnvSchemaType =
-    | BooleanConstructor
-    | NumberConstructor
-    | StringConstructor
-    | RegExp
-    | Array<string>;
-
-interface OptionalKeyConfig {
-    type: EnvSchemaType;
-    optional: true;
-}
-
-interface BooleanKeyConfig {
-    type: BooleanConstructor;
-    default: boolean;
-}
-
-interface NumberKeyConfig {
-    type: NumberConstructor;
-    default: number;
-}
-
-interface StringKeyConfig {
-    type: StringConstructor | RegExp | Array<string>;
-    default: string;
-}
-
-export type EnvSchemaValue =
-    | EnvSchemaType
-    | OptionalKeyConfig
-    | BooleanKeyConfig
-    | NumberKeyConfig
-    | StringKeyConfig;
+export type EnvSchema = {
+    [key: string]: EnvSchemaValue;
+};
 
 export interface EnvKeyConfig {
     type: EnvSchemaType;
@@ -42,9 +12,39 @@ export interface EnvKeyConfig {
     default?: boolean | number | string;
 }
 
-export type EnvSchema = {
-    [key: string]: EnvSchemaValue;
-};
+export type EnvSchemaValue = DefaultValueKeyConfig | OptionalKeyConfig | EnvSchemaType;
+
+export type EnvSchemaType =
+    | BooleanConstructor
+    | NumberConstructor
+    | StringConstructor
+    | RegExp
+    | Array<string>;
+
+interface OptionalKeyConfig<T extends EnvSchemaType = EnvSchemaType> {
+    type: T;
+    optional: boolean;
+}
+
+type DefaultValueKeyConfig =
+    | DefaultBooleanKeyConfig
+    | DefaultNumberKeyConfig
+    | DefaultStringKeyConfig;
+
+interface DefaultBooleanKeyConfig {
+    type: BooleanConstructor;
+    default: boolean;
+}
+
+interface DefaultNumberKeyConfig {
+    type: NumberConstructor;
+    default: number;
+}
+
+interface DefaultStringKeyConfig {
+    type: StringConstructor | RegExp | Array<string>;
+    default: string;
+}
 
 /**
  * Resolves to the type of the provided environment schema.
@@ -52,15 +52,31 @@ export type EnvSchema = {
  * @typeParam S - pass `typeof schema` here
  */
 export type EnvType<S extends EnvSchema> = {
-    [K in keyof S]: S[K] extends BooleanConstructor
-        ? boolean
-        : S[K] extends NumberConstructor
-        ? number
-        : S[K] extends StringConstructor
-        ? string
-        : S[K] extends RegExp
-        ? string
-        : S[K] extends Array<infer U>
-        ? U
+    [K in keyof S]: S[K] extends DefaultValueKeyConfig
+        ? EnvSchemaDefaultValueType<S[K]>
+        : S[K] extends OptionalKeyConfig<infer U>
+        ? EnvSchemaValueType<S[K]['type']>
+        : S[K] extends EnvSchemaType
+        ? EnvSchemaValueType<S[K]>
         : never;
 };
+
+type EnvSchemaDefaultValueType<C extends DefaultValueKeyConfig> = C extends DefaultBooleanKeyConfig
+    ? boolean
+    : C extends DefaultNumberKeyConfig
+    ? number
+    : C extends DefaultStringKeyConfig
+    ? string
+    : never;
+
+type EnvSchemaValueType<T extends EnvSchemaType> = T extends BooleanConstructor
+    ? boolean
+    : T extends NumberConstructor
+    ? number
+    : T extends StringConstructor
+    ? string
+    : T extends RegExp
+    ? string
+    : T extends Array<infer U>
+    ? U
+    : never;
