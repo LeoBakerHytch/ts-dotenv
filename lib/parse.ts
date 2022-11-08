df366c7b79d0e35ceceb0ee6386a60777d554642
Leo <l.bakerhytch@gmail.com>
2022-11-08 03:28:39 +0000

    Add support for single- and double-quoted string values

--- lib/parse.ts
+++ lib/parse.ts
@@ -19,7 +19,21 @@ export function parse(dotEnv: string): Env {
         if (!match) continue;
 
         const variableName = match[1];
-        parsed[variableName] = match[2];
+
+        // Adapted from https://github.com/motdotla/dotenv/blob/463952012640a919a82be0de11f473c1224b498a/lib/main.js
+        let value = match[2].trim() || '';
+        const maybeQuote = value[0];
+
+        // Remove surrounding quotes
+        value = value.replace(/^(['"])([\s\S]*)\1$/gm, '$2');
+
+        // Expand newlines if double quoted
+        if (maybeQuote === '"') {
+            value = value.replace(/\\n/g, '\n');
+            value = value.replace(/\\r/g, '\r');
+        }
+
+        parsed[variableName] = value;
     }
 
     return parsed;
--- tests/parse.test.ts
+++ tests/parse.test.ts
@@ -15,14 +15,59 @@ describe('parse', () => {
         });
     });
 
-    it('should parse large number values', () => {
+    it('should parse string values with leading and trailing whitespace removed', () => {
         expect(parse(ok3)).toEqual({
+            STRING_LEADING_WHITESPACE: 'Lorem ipsum',
+            STRING_TRAILING_WHITESPACE: 'Lorem ipsum',
+            STRING_SURROUNDING_WHITESPACE: 'Lorem ipsum',
+        });
+    });
+
+    it('should parse single and double quoted string values', () => {
+        expect(parse(ok4)).toEqual({
+            STRING_SINGLE_QUOTED: 'Lorem ipsum',
+            STRING_DOUBLE_QUOTED: 'Lorem ipsum',
+        });
+    });
+
+    it('should preserve leading or trailing whitespace within quoted string values', () => {
+        expect(parse(ok5)).toEqual({
+            STRING_SINGLE_QUOTED_WHITESPACE: ' Lorem ipsum ',
+            STRING_DOUBLE_QUOTED_WHITESPACE: ' Lorem ipsum ',
+        });
+    });
+
+    it('should remove whitespace outside quoted string values', () => {
+        expect(parse(ok6)).toEqual({
+            STRING_QUOTED_LEADING_WHITESPACE: 'Lorem ipsum',
+            STRING_QUOTED_TRAILING_WHITESPACE: 'Lorem ipsum',
+            STRING_QUOTED_SURROUNDING_WHITESPACE: 'Lorem ipsum',
+        });
+    });
+
+    it('should replace escaped newlines / carriage returns with their literal value inside double quotes', () => {
+        expect(parse(ok7)).toEqual({
+            STRING_DOUBLE_QUOTED_NEWLINE: 'Lorem\nipsum',
+            STRING_DOUBLE_QUOTED_CARRIAGE_RETURN: 'Lorem\ripsum',
+        });
+    });
+
+    it('should parse empty strings', () => {
+        expect(parse(ok8)).toEqual({
+            EMPTY_STRING: '',
+            EMPTY_SINGLE_QUOTED_STRING: '',
+            EMPTY_DOUBLE_QUOTED_STRING: '',
+        });
+    });
+
+    it('should parse large number values', () => {
+        expect(parse(ok9)).toEqual({
             LARGE_NUMBER: '123456789012345',
         });
     });
 
     it('should parse lowercase identifiers', () => {
-        expect(parse(ok4)).toEqual({
+        expect(parse(ok10)).toEqual({
             lowercase_identifier: 'true',
         });
     });
@@ -51,10 +96,43 @@ STRING_WITH_WHITESPACE=Lorem ipsum
 `;
 
 const ok3 = `
-LARGE_NUMBER=123456789012345
+STRING_LEADING_WHITESPACE= Lorem ipsum
+STRING_TRAILING_WHITESPACE=Lorem ipsum 
+STRING_SURROUNDING_WHITESPACE= Lorem ipsum 
 `;
 
 const ok4 = `
+STRING_SINGLE_QUOTED='Lorem ipsum'
+STRING_DOUBLE_QUOTED="Lorem ipsum"
+`;
+
+const ok5 = `
+STRING_SINGLE_QUOTED_WHITESPACE=' Lorem ipsum '
+STRING_DOUBLE_QUOTED_WHITESPACE=" Lorem ipsum "
+`;
+
+const ok6 = `
+STRING_QUOTED_LEADING_WHITESPACE= 'Lorem ipsum'
+STRING_QUOTED_TRAILING_WHITESPACE='Lorem ipsum' 
+STRING_QUOTED_SURROUNDING_WHITESPACE= 'Lorem ipsum' 
+`;
+
+const ok7 = `
+STRING_DOUBLE_QUOTED_NEWLINE="Lorem\\nipsum"
+STRING_DOUBLE_QUOTED_CARRIAGE_RETURN="Lorem\\ripsum"
+`;
+
+const ok8 = `
+EMPTY_STRING=
+EMPTY_SINGLE_QUOTED_STRING=''
+EMPTY_DOUBLE_QUOTED_STRING=""
+`;
+
+const ok9 = `
+LARGE_NUMBER=123456789012345
+`;
+
+const ok10 = `
 lowercase_identifier=true
 `;
 
